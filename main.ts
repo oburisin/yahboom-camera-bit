77ba119a87273f5a95ba88f91165a35909c918e4
oburisin <15708671+oburisin@users.noreply.github.com>
2025-10-13 02:24:39 +0200

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,13 @@
+input.onButtonPressed(Button.A, function () {
+    s1 += 5
+})
+input.onButtonPressed(Button.B, function () {
+    s1 += -5
+})
+let s1 = 90
+let s2 = 90
+SuperBit.Servo2(SuperBit.enServo.S1, s1)
+SuperBit.Servo2(SuperBit.enServo.S2, s2)
 basic.forever(function () {
-	
+    SuperBit.Servo2(SuperBit.enServo.S1, s1)
 })
