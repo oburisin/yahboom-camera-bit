e4620a6064df755375e9a75c524961051b7f090e
oburisin <15708671+oburisin@users.noreply.github.com>
2025-11-30 12:24:54 +0100

    Test servo limits

--- main.ts
+++ main.ts
@@ -1,13 +1,33 @@
 input.onButtonPressed(Button.A, function () {
-    s1 += 5
+    temp_s1 = s1
+    temp_s1 += 45
+    if (temp_s1 < 0) {
+        temp_s1 = 0
+        basic.showNumber(temp_s1)
+    }
+    s1 = temp_s1
+    SuperBit.Servo2(SuperBit.enServo.S1, s1)
+})
+input.onButtonPressed(Button.AB, function () {
+    s1 = 135
+    basic.showNumber(s1)
+    SuperBit.Servo2(SuperBit.enServo.S1, s1)
 })
 input.onButtonPressed(Button.B, function () {
-    s1 += -5
-})
-let s1 = 90
-let s2 = 90
-SuperBit.Servo2(SuperBit.enServo.S1, s1)
-SuperBit.Servo2(SuperBit.enServo.S2, s2)
-basic.forever(function () {
+    temp_s1 = s1
+    temp_s1 += -45
+    if (temp_s1 > 270) {
+        temp_s1 = 270
+        basic.showNumber(temp_s1)
+    }
+    s1 = temp_s1
     SuperBit.Servo2(SuperBit.enServo.S1, s1)
+})
+let temp_s1 = 0
+let s1 = 0
+basic.showIcon(IconNames.Happy)
+s1 = 135
+SuperBit.Servo2(SuperBit.enServo.S1, s1)
+basic.forever(function () {
+	
 })
