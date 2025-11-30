118075800c0c43dc75b1be04f3780c60d0b6dc65
oburisin <15708671+oburisin@users.noreply.github.com>
2025-11-30 13:48:23 +0100

    Control both servos with buttons

--- main.ts
+++ main.ts
@@ -1,33 +1,58 @@
 input.onButtonPressed(Button.A, function () {
     temp_s1 = s1
-    temp_s1 += 45
+    temp_s1 += -5
     if (temp_s1 < 0) {
         temp_s1 = 0
-        basic.showNumber(temp_s1)
+        basic.showLeds(`
+            . . . . .
+            # . . . .
+            # . . . .
+            # . . . .
+            . . . . .
+            `)
     }
     s1 = temp_s1
-    SuperBit.Servo2(SuperBit.enServo.S1, s1)
+    SuperBit.Servo(SuperBit.enServo.S1, s1)
+    SuperBit.Servo(SuperBit.enServo.S2, s1)
 })
 input.onButtonPressed(Button.AB, function () {
-    s1 = 135
-    basic.showNumber(s1)
-    SuperBit.Servo2(SuperBit.enServo.S1, s1)
+    s1 = 90
+    basic.showLeds(`
+        . . . . .
+        . . # . .
+        . . # . .
+        . . # . .
+        . . . . .
+        `)
+    SuperBit.Servo(SuperBit.enServo.S1, s1)
+    SuperBit.Servo(SuperBit.enServo.S2, s1)
 })
 input.onButtonPressed(Button.B, function () {
     temp_s1 = s1
-    temp_s1 += -45
-    if (temp_s1 > 270) {
-        temp_s1 = 270
-        basic.showNumber(temp_s1)
+    temp_s1 += 5
+    if (temp_s1 > 180) {
+        temp_s1 = 180
+        basic.showLeds(`
+            . . . . .
+            . . . . #
+            . . . . #
+            . . . . #
+            . . . . .
+            `)
     }
     s1 = temp_s1
-    SuperBit.Servo2(SuperBit.enServo.S1, s1)
+    SuperBit.Servo(SuperBit.enServo.S1, s1)
+    SuperBit.Servo(SuperBit.enServo.S2, s1)
+})
+input.onGesture(Gesture.Shake, function () {
+	
 })
 let temp_s1 = 0
 let s1 = 0
 basic.showIcon(IconNames.Happy)
-s1 = 135
-SuperBit.Servo2(SuperBit.enServo.S1, s1)
+s1 = 90
+SuperBit.Servo(SuperBit.enServo.S1, s1)
+SuperBit.Servo(SuperBit.enServo.S2, s1)
 basic.forever(function () {
 	
 })
